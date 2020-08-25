1af8fdadd48eac9ad1d3a2d05b57ba9a06a3ac41
Truong Giang NGUYEN PHAM <tnguyen@MacBook-Air-de-Truong.local>
2020-08-25 16:21:08 +0200

    FINAL

--- src/app/book-list/single-book/single-book.component.ts
+++ src/app/book-list/single-book/single-book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/models/Book.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BooksService } from 'src/app/services/books.service';
 
 @Component({
   selector: 'app-single-book',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./single-book.component.scss']
 })
 export class SingleBookComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+  
+  book: Book;
+  
+  constructor(private route: ActivatedRoute, private booksService: BooksService,
+    private router: Router) {}
+    
+    ngOnInit() {
+      this.book = new Book('', '');
+      const id = this.route.snapshot.params['id'];
+      this.booksService.getSingleBook(+id).then(
+        (book: Book) => {
+          this.book = book;
+        }
+        );
+      }
+      
+      onBack() {
+        this.router.navigate(['/books']);
+      }
+    }
